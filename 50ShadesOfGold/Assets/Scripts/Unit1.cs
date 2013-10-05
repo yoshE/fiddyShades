9c498eac8ce8de0693241e7db1f3df1bc89c3bb4
Yoshua Lo <yoshualo@usc.edu>
2013-10-05 04:26:12 -0700

    unit1 code
    added unit1 code and a lot of imports

---
+++ 50ShadesOfGold/Assets/Scripts/Unit1.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Unit1 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.D)){
+			transform.rigidbody.velocity = new Vector3(10,rigidbody.velocity.y,0);
+		}
+		if(Input.GetKey(KeyCode.W))
+		{
+			transform.rigidbody.velocity = new Vector3(rigidbody.velocity.x,10,0);
+		}
+	}
+}
